14857e47b7d7b3af142accb97aa89c4db2e7cec7
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-17 00:47:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ function run(){
   var code=document.getElementById("program").value;
   var output='';var p=0;var s=[];var ts=[];m=0
   while (code.length>p){
-    if (code[p]=='"'){m=1};
-    if (m==1){p++;p++;ts.push(code[p]) ; if (code[p]=='"'){m=0;break};};
+    if (code[p]=='"'){m=1;p++};
+    if (m==1){ts.push(code[p]) ; if (code[p]=='"'){m=0;break};};
     p++
   }
   output=output+'\nSTACK: ['+s+']\nTEMPSTACK: ['+ts+']'
