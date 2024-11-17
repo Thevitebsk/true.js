650701d9e6d318021f9219592210a2d5e3203b20
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-17 14:52:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ function run(){
   var output='';var p=0;var s=[];var ts=[];m=0
   while (code.length>p){
     if (code[p]=='"'){m=1;p++};
-    if (m==1){ts.push(code[p]) ; if (code[p]=='"'){m=0;ts.pop();while(ts.length!==1){a=(ts[0])+(ts[1])
-    ts.pop(0);ts.pop(0);ts.reverse();ts.push(a);ts.reverse()};s.push(ts)};};
+    if (m==1){ts.push(code[p]) ; if (code[p]=='"'){m=0;ts.pop();ts=[ts.join()];};};
     p++
   }
   output=output+'\nSTACK: ['+s+']\nTEMPSTACK: ['+ts+']\nBYTES: '+code.length
