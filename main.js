52801db0230280cd7be695c64360becd2f10f7ba
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-17 22:41:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var global_end
+var global_end=0;var p=0;
 function run(){
   var code=document.getElementById("program").value;
-  var output='';var p=0;var s=[];var ts=[];m=0;let stringcon="";var num=["0","1","2","3","4","5","6","7","8","9"];var input=document.getElementById("input").value;
+  var output='';var s=[];var ts=[];m=0;let stringcon="";var num=["0","1","2","3","4","5","6","7","8","9"];var input=document.getElementById("input").value;
   while (code.length>p){
     //IN PROGRESS
     if (code[p]=='"'){m=1;p++};
