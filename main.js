6cab7f7777f5a9d51930f9ab232303c2fafe1dd1
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-17 14:42:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function run(){
     if (code[p]=='"'){m=1;p++};
     if (m==1){ts.push(code[p]) ; if (code[p]=='"'){m=0;break};
     while(ts.length>1){a=(ts[0])+(ts[1])
-    ts.pop(0);ts.pop(0);ts.reverse();ts.push(a);ts.reverse()};};
+    ts.pop(0);ts.pop(0);ts.reverse();ts.push(a);ts.reverse()};ts.pop();s.push(ts)};
     p++
   }
   output=output+'\nSTACK: ['+s+']\nTEMPSTACK: ['+ts+']\nBYTES: '+code.length
