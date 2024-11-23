5e147412288b8c773f6c43933f1b2012839b9100
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-24 01:21:38 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var global_end=0;p=0;
 function run(){
   var code=document.getElementById("program").value;
-  var output='';var s=[];var ts=[];m=0;let stringcon="";var num=["0","1","2","3","4","5","6","7","8","9"]
+  var output='';var s=[];var ts=[];m=0var num=["0","1","2","3","4","5","6","7","8","9"]
   var input=document.getElementById("input").value;var input = input.split("\n");var tv=""
   while (code.length>p, code[p]!=="\n"){
     //IN PROGRESS
     if (code[p]=='"'){m=1;p++};
-    if (m==1){ts.push(code[p]) ; if (code[p]=='"'){m=0;ts.pop();var ts=ts.join('');for(let i=0; i<tempStack.length; i++){stringcon+=ts[0]};alert(stringcon);};};
+    if (m==1){ts.push(code[p]) ; if (code[p]=='"'){m=0;ts.pop();};
     //IN PROGRESS
     if (code[p]in num){s.push(code[p])}
     else if (code[p]=="."){if (s.length>0){output+=s.pop()} else {error("STACK EMPTY");break};}
