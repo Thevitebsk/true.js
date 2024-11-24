052ad83a19d94ab74812cf79e3f30ba68829e4a0
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-24 13:59:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function main(){
     else if (code[p]=="."){if (s.length>0){output+=s.pop()} else {break};}
     else if (code[p]==","){tv=input[0];input.shift()}
     else if (code[p]=="\n"){break}
-    else if (code[p]=="'"){s.push(tv.substring(1,tv.lenght))}
+    else if (code[p]=="'"){s.push(tv.substring(tv.lenght,1))}
     p++
   }
   fo=output+'\nSTACK: ['+s+']\nTEMPSTACK: ['+ts+']\nBYTES: '+code.length+'\nINPUT: '+tv
