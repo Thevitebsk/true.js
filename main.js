2220a692700bfd2c6584e05aee9f45a6677521ed
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-24 13:56:24 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var global_end=0;p=0;
 function main(){
   p=0;var code=document.getElementById("program").value;
   var output='';var s=[];var ts=[];m=0;var num=["0","1","2","3","4","5","6","7","8","9"]
-  var input=document.getElementById("input").value;var input = input.split("\n");var tv=""
+  var input=document.getElementById("input").value;var input = input.split("\n");var tv="";e=""
   while (code.length>p){
     //IN PROGRESS
     if (code[p]=='"'){m=1;p++};
@@ -12,7 +12,7 @@ function main(){
     else if (code[p]=="."){if (s.length>0){output+=s.pop()} else {break};}
     else if (code[p]==","){tv=input[0];input.shift()}
     else if (code[p]=="\n"){break}
-    else if (code[p]=="'"){tv=tv.substring(1,tv.lenght-1)}
+    else if (code[p]=="'"){e=tv.substring(1,tv.lenght-1);tv=e;e=""}
     p++
   }
   fo=output+'\nSTACK: ['+s+']\nTEMPSTACK: ['+ts+']\nBYTES: '+code.length+'\nINPUT: '+tv
