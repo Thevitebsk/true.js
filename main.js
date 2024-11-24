ef927510358c42bc4cc26901c0ae1158e85ec371
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-24 11:54:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var global_end=0;p=0;
-function run(){
+function main(){
   var code=document.getElementById("program").value;
   var output='';var s=[];var ts=[];m=0var num=["0","1","2","3","4","5","6","7","8","9"]
   var input=document.getElementById("input").value;var input = input.split("\n");var tv=""
